9a5e116678afacefde879460a896470b428e569a
xtang <2111803140@qq.com>
2026-08-12 19:57:46 +0800

    修复连接bug

--- js/main.js
+++ js/main.js
@@ -12,10 +12,11 @@
    window.NetPlay.on('onMove', function (r, c, player) {
      window.Gomoku.remoteMove(r, c, player);
    });
-   window.NetPlay.on('onRestart', function () {
-     // 双方同意重开：重新开始当前对局
-     window.Gomoku.startGame({ mode: 'net', netSide: window.NetPlay.getState().mySide });
-   });
+    window.NetPlay.on('onRestart', function () {
+      // 重开后回联机面板等待双方再次准备
+      window.Gomoku.backToMenu();
+      window.Cover.show();
+    });
    window.NetPlay.on('onOpponentLeft', function () {
      window.Gomoku.backToMenu();
      alert('对手已离开房间');
